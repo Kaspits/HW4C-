0dadecbf68d3298192ad0d9e9351a39d42a5f86d
Kaspits <azdanova97@gmail.com>
2022-12-08 13:43:57 +0100

    Add new homeWork

---
+++ HOMEWORK4/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+// Получение числа
+// int GetNumber(string message)
+// {
+//     bool isCorrect = false;
+//     int result = 0;
+
+//     while (!isCorrect)
+//     {
+//         Console.WriteLine(message);
+//         if (int.TryParse(Console.ReadLine(), out result))
+//         {
+//             isCorrect = true;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Ввели не число");
+//         }
+//     }
+//     return result;
+// }
+
+// int GetSumm(int numberA, int numberB)
+// {
+//     int summ = 1;
+//     for (int i = 1; i <= numberB; i++)
+//     {
+//         summ *= numberA;
+//     }
+//     return summ;
+// }
+
+// int numberA = GetNumber("Введите число A");
+// int numberB = GetNumber("Введите число B");
+
+// int summ = GetSumm(numberA, numberB);
+
+// Console.WriteLine($"Натуральная степень {numberA} = {summ}");
+
+// ...................................................................................
+/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+// Console.WriteLine("введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+// while (a > 0)
+// {
+//     count += a % 10;
+//     a /= 10;
+// }
+// Console.WriteLine($"Сумма цифр в числе = {count}");
+
+/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] array = new int[8];
+Random rnd = new Random();
+
+for (long i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 20);
+    Console.Write($"{array[i]}, ");
+}
+
+Console.WriteLine();
---
+++ task_1/Program.cs
@@ -0,0 +1,92 @@
+﻿// напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+// Получение числа
+// int GetNumber(string message)
+// {
+//     bool isCorrect = false;
+//     int result = 0;
+
+//     while (!isCorrect)
+//     {
+//         Console.WriteLine(message);
+//         if (int.TryParse(Console.ReadLine(), out result))
+//         {
+//             isCorrect = true;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Ввели не число");
+//         }
+//     }
+//     return result;
+// }
+
+// int GetSumm(int number)
+// {
+//     int summ = 0;
+//     for (int i = 1; i <= number; i++)
+//     {
+//         summ += i;
+//     }
+//     return summ;
+// }
+
+// int number = GetNumber("Введите число");
+// int summ = GetSumm(number);
+
+// Console.WriteLine($"Сумма чисел от 1 до {number} = {summ}");
+
+
+// ...........................................................................
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.WriteLine("введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+// while (a > 0)
+// {
+//     a = a / 10;
+//     count++;
+// }
+
+// Console.WriteLine($"Количество цифр в числе = {count}");
+
+// .........................................................................
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+// Получение числа
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int num = GetNumber("Введите число:");
+int result = 1;
+
+for (int i = 1; i <= num; i++)
+{
+    result *= i;
+}
+
+Console.WriteLine($"Производства от 1 до {num} равно {result}");
